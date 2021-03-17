fdb33368d1c083af70fafefbc9e3b3f982b9bb3f
BilinJossy <bilinjossy@gmail.com>
2021-03-17 11:52:56 +0530

    Message

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col col-2 col-sm-2 col-md-3 col-lg-3 col-xl-3"></div>
+            <div class="col col-8 col-sm-8 col-md-6 col-lg-6 col-xl-6">
+            <table class="table">
+                <tr>
+                    <td>NAME</td>
+                    <td><input type="text" class="form-control"></td>
+                </tr>
+                <tr>
+                    <td>ROLL NO</td>
+                    <td><input type="text" class="form-control"></td>
+                </tr>
+                <tr>
+                    <td>ADMISSION NO</td>
+                    <td><input type="text" class="form-control"></td>
+                </tr>
+                <tr>
+                    <td>COLLEGE</td>
+                    <td><input type="text" class="form-control"></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td><button class="btn btn-outline-success">SUBMIT</button></td>
+                </tr>
+            </table>
+            </div>
+            <div class="col col-2 col-sm-2 col-md-3 col-lg-3 col-xl-3"></div>
+        </div>
+    </div>
+</body>
+</html>
